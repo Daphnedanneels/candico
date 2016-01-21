ef0cf0d9c0c616eb91d763b72e3cd031d0ab502a
Daphnedanneels <daphne.danneels@student.howest.be>
2016-01-22 00:47:52 +0100

    rare commit nodig?

---
+++ _js/classes/states/Preload.js
@@ -0,0 +1,29 @@
+export default class Preload extends Phaser.State{
+	preload(){
+		this.load.atlasJSONHash('windroos', 'assets/images/windroos.png', 'assets/data/windroos.json');
+		this.load.image('wiel', 'assets/images/wiel.png');
+		this.load.image('waterbackground', 'assets/images/waterbackground.png');
+		this.load.image('watersprite', 'assets/images/watersprite.png');
+		this.load.image('cloudsprite', 'assets/images/clouds01.png');
+		this.load.image('background', 'assets/images/background.png');
+		this.load.image('frame', 'assets/web/frame.png');
+		this.load.image('button', 'assets/web/button.png');
+		this.load.image('water', 'assets/images/water.png');
+		this.load.image('sail', 'assets/images/sail.png');
+		this.load.image('sun', 'assets/images/zon.png');
+		this.load.image('ship', 'assets/images/ship2.png');
+		this.load.spritesheet('buttons', 'assets/images/buttons.png', 50, 50, 2);
+		this.load.image('navigatie-01', 'assets/images/navigatie-01.png');
+		this.load.image('navigatie-02', 'assets/images/navigatie-02.png');
+		this.load.image('navigatie-03', 'assets/images/navigatie-03.png');
+	}
+
+	create(){
+		console.log('preload');
+		this.game.state.start('Menu');
+	}
+	
+	update(){
+
+	}
+}
--- js/script.js
+++ js/script.js
@@ -40,69 +40,34 @@
 /******/ 	return __webpack_require__(0);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-	var _classesStatesMain = __webpack_require__(1);
-
-	var _classesStatesMain2 = _interopRequireDefault(_classesStatesMain);
-
-	var init = function init() {
-		var game = new Phaser.Game(800, 600, Phaser.AUTO);
-		game.state.add('Main', _classesStatesMain2['default'], false);
-		game.state.start('Main');
-	};
-
-	init();
+	eval("__webpack_require__(1);\nmodule.exports = __webpack_require__(12);\n\n\n/*****************\n ** WEBPACK FOOTER\n ** multi main\n ** module id = 0\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///multi_main?");
 
 /***/ },
-/* 1 */
+
+/***/ 1:
+/***/ function(module, exports, __webpack_require__) {
+
+	eval("'use strict';\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }\n\nvar _classesStatesMain = __webpack_require__(3);\n\nvar _classesStatesMain2 = _interopRequireDefault(_classesStatesMain);\n\nvar init = function init() {\n\tvar game = new Phaser.Game(800, 600, Phaser.AUTO);\n\tgame.state.add('Main', _classesStatesMain2['default'], false);\n\tgame.state.start('Main');\n};\n\ninit();\n\n/*****************\n ** WEBPACK FOOTER\n ** ./_js/script.js\n ** module id = 1\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./_js/script.js?");
+
+/***/ },
+
+/***/ 3:
 /***/ function(module, exports) {
 
-	"use strict";
+	eval("\"use strict\";\n\nObject.defineProperty(exports, \"__esModule\", {\n\tvalue: true\n});\n\nvar _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();\n\nvar _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if (\"value\" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _inherits(subClass, superClass) { if (typeof superClass !== \"function\" && superClass !== null) { throw new TypeError(\"Super expression must either be null or a function, not \" + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }\n\nvar Main = (function (_Phaser$State) {\n\t_inherits(Main, _Phaser$State);\n\n\tfunction Main() {\n\t\t_classCallCheck(this, Main);\n\n\t\t_get(Object.getPrototypeOf(Main.prototype), \"constructor\", this).apply(this, arguments);\n\t}\n\n\t_createClass(Main, [{\n\t\tkey: \"preload\",\n\t\tvalue: function preload() {}\n\t}, {\n\t\tkey: \"create\",\n\t\tvalue: function create() {}\n\t}, {\n\t\tkey: \"update\",\n\t\tvalue: function update() {}\n\t}]);\n\n\treturn Main;\n})(Phaser.State);\n\nexports[\"default\"] = Main;\nmodule.exports = exports[\"default\"];\n\n/*****************\n ** WEBPACK FOOTER\n ** ./_js/classes/states/Main.js\n ** module id = 3\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./_js/classes/states/Main.js?");
 
-	Object.defineProperty(exports, "__esModule", {
-		value: true
-	});
+/***/ },
 
-	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+/***/ 12:
+/***/ function(module, exports) {
 
-	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var Preload = (function (_Phaser$State) {
-		_inherits(Preload, _Phaser$State);
-
-		function Preload() {
-			_classCallCheck(this, Preload);
-
-			_get(Object.getPrototypeOf(Preload.prototype), "constructor", this).apply(this, arguments);
-		}
-
-		_createClass(Preload, [{
-			key: "preload",
-			value: function preload() {}
-		}, {
-			key: "create",
-			value: function create() {}
-		}, {
-			key: "update",
-			value: function update() {}
-		}]);
-
-		return Preload;
-	})(Phaser.State);
-
-	exports["default"] = Preload;
-	module.exports = exports["default"];
+	eval("// removed by extract-text-webpack-plugin\n\n/*****************\n ** WEBPACK FOOTER\n ** ./_scss/style.scss\n ** module id = 12\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./_scss/style.scss?");
 
 /***/ }
-/******/ ]);
+
+/******/ });
